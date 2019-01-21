d1332cf0e6fade852d8558fab12719475c847a2d
lbonaldo <bonaldo.luca12@gmail.com>
2019-01-21 23:23:57 +0100

    Changed the plot interval from [-5.0; 5.0] to [-3.0; 3.0]

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-xval = [i for i in np.arange(-5.0, 5.1, 0.1) if round(i, 1) != 0.1]
+xval = [i for i in np.arange(-3.0, 3.1, 0.1) if round(i, 1) != 0.1]
 
 yval = []
 
@@ -12,10 +12,7 @@ usage = (
     '''Usage: script.py [FUNCTION FORM]
 Return a function in the domain [-5.0, 5.0] whose form depends on the argument given as an input:
 
-    1     f(x) = x
-    2     f(x) = x**2
-    3     f(x) = x**3
-    4     f(x) = tan(x)'''
+    1     f(x) = x'''
 )
 
 if len(sys.argv) < 2:
