0fd0d80569655ffc03ae4cd9fac85a8901eeb86e
lbonaldo <bonaldo.luca12@gmail.com>
2019-01-21 17:52:04 +0100

    list fill

--- script.py
+++ script.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
 import sys
+import numpy as np
 
-sys.argv[0]
+xval = [i for i in np.arange(-5.0, 5.1, 0.1) if round(i, 1) != 0.1]
+
+yval = []
+
+if sys.argv[0] == 1:
+    for i in xval:
+        yval.append(i)
