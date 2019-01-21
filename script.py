e4851460ca0ca9836a63084dbd734f5ac4b4467e
lbonaldo <bonaldo.luca12@gmail.com>
2019-01-21 22:12:23 +0100

    introduced new functions: if argument is 2: f(x)=x**2, if argument is 3: f(x)=x**3

--- script.py
+++ script.py
@@ -8,9 +8,15 @@ xval = [i for i in np.arange(-5.0, 5.1, 0.1) if round(i, 1) != 0.1]
 
 yval = []
 
-if sys.argv[1] == "1":
-    for i in xval:
+for i in xval:
+    if sys.argv[1] == "1":
         yval.append(i)
+
+    elif sys.argv[1] == "2":
+        yval.append(i**2)
+
+    elif sys.argv[1] == "3":
+        yval.append(i**3)
 
 plt.plot(xval, yval)
 plt.show()
