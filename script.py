e1a8c955dbbb5a29ba69311f3c2e2e1382670177
lbonaldo <bonaldo.luca12@gmail.com>
2019-01-21 22:48:58 +0100

    Added a support for the usage of the script which is given when the script is run without arguments.

--- script.py
+++ script.py
@@ -8,9 +8,23 @@ xval = [i for i in np.arange(-5.0, 5.1, 0.1) if round(i, 1) != 0.1]
 
 yval = []
 
-if sys.argv[1] == "1":
-    for i in xval:
-        yval.append(i)
+usage = (
+    '''Usage: script.py [FUNCTION FORM]
+Return a function in the domain [-5.0, 5.0] whose form depends on the argument given as an input:
 
-plt.plot(xval, yval)
-plt.show()
+    1     f(x) = x
+    2     f(x) = x**2
+    3     f(x) = x**3
+    4     f(x) = tan(x)'''
+)
+
+if len(sys.argv) < 2:
+    print(usage)
+
+else:
+    for i in xval:    
+        if sys.argv[1] == "1":
+            yval.append(i)
+  
+    plt.plot(xval, yval)
+    plt.show()
