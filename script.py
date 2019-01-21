bd2ccde9c9abfe3361eafe88e987c54c0b657adb
lbonaldo <bonaldo.luca12@gmail.com>
2019-01-21 18:04:18 +0100

    plot added

--- script.py
+++ script.py
@@ -2,11 +2,15 @@
 
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
 
 xval = [i for i in np.arange(-5.0, 5.1, 0.1) if round(i, 1) != 0.1]
 
 yval = []
 
-if sys.argv[0] == 1:
+if sys.argv[1] == "1":
     for i in xval:
         yval.append(i)
+
+plt.plot(xval, yval)
+plt.show()
